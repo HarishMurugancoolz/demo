723880c9646cd1e1409ab0374cd8f1b95d595dca
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-09-12 12:13:19 +0530

    Update sender-test.cpp

--- sender-test/sender-test.cpp
+++ sender-test/sender-test.cpp
@@ -1,6 +1,5 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include "catch.hpp"
+#define CATCH_CONFIG_MAIN
+#include"catch.hpp"
 #include"fileArray.h"
 #include"ColumnFilter.h"
 
@@ -10,15 +9,30 @@
 #include<fstream>
 using namespace std;
 
-TEST_CASE("when stringstream is given then report it in vector")
+TEST_CASE("when stringstream is given then report it in vector form of string")
 {
 	string sentence = "hello how are you";
 	vector<string>row = { "hello how are you" };
 	stringstream s(sentence);
-	FileArray::FileToArrayCreator ftac;
+	ifstream fin;
+	FileArray::FileArrayCreator ftac(fin);
 	REQUIRE(ftac.pushWordsToRowVector(s)== row);
 
 }
+TEST_CASE("when an empty csv File is given  then report the file in empty vector")
+{
+	vector<vector<string>> csv_vector = {};
+	ofstream fout;
+	fout.open("test1.csv", ios::out | ios::trunc);
+	fout.close();
+	ifstream fin;
+	fin.open("test1.csv", ios::in);
+	FileArray::FileArrayCreator ftac(fin);
+	REQUIRE(ftac.getFileArray() == csv_vector);
+
+
+}
+
 TEST_CASE("when csv File is given  then report the file in vector form" )
 {
 	vector<vector<string>> csv_vector = { {"what","good","how"},{"a","modify","nice"},{"great","readme","this"},{"session","","is"} };
@@ -43,8 +57,8 @@ TEST_CASE("when csv File is given  then report the file in vector form" )
 	fout.close();
 	ifstream fin;
 	fin.open("test.csv", ios::in);
-	FileArray::FileToArrayCreator ftac;
-	REQUIRE(ftac.createFileArray(fin) == csv_vector);
+	FileArray::FileArrayCreator ftac(fin);
+	REQUIRE(ftac.getFileArray() == csv_vector);
 
 
 }
@@ -52,9 +66,9 @@ TEST_CASE("when the column number and 2D vector of strings"
 	"are given then report vector with corresponding column of strings")
 {
 	vector<vector<string>> csv_vector = { {"what","good","how"},{"a","modify","nice"},{"great","readme","this"},{"session","","is"} };
-	columnFilter::columnFilter CF;
+	ColumnFilter::ColumnFilter CF(csv_vector, 2);
 	vector<string> expectedColumn = { "good", "modify", "readme","" };
-	REQUIRE(CF.getColumn(csv_vector,2) == expectedColumn);
+	REQUIRE(CF.getColumn() == expectedColumn);
 }
 
 
